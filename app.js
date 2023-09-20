8b3a352906ef5803a5ad09b148229ca4b77d16f9
Miguel guerrero <uannokaiserfrom@gmail.com>
2023-09-19 21:13:49 -0300

    final commit

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 import express from "express";
 import cors from "cors";
+import { createJsonApi, saveQueryBack } from "send-http-axios-doc";
 const app = express();
 const port = 3000;
 
 //config
 app.use(express.json());
-
+app.use(express.static("send"));
 app.use(cors("*"));
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
   console.log("hola mundo");
   res.send("hola estoy aqui miguel");
 });
-app.post("/", (req, res) => {
+app.post("/home", (req, res) => {
   const data = req.body;
   res.send(data);
 });
 
+app.use("/saveQuery", saveQueryBack);
+createJsonApi(app, port);
 app.listen(port, () => {
   console.log("server runin on port: " + port);
 });
